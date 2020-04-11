865815ff8cc9775d807e0188840e6f236bd4f807
adrianong1 <adrianong@cmail.carleton.ca>
2020-04-11 17:11:35 -0400

    created files

--- bookstore.py
+++ bookstore.py
@@ -1,1 +1,1 @@
-print("hello")
+# This file will contain the main logic of the store
---
+++ query_functions.py
@@ -0,0 +1,4 @@
+"""
+This file will contain sql queries turned into python functions
+that we can call in bookstore.py
+"""
